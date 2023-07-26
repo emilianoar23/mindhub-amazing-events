58b6d220403b45febcafcb9ca85cf9168934490f
emilianoar23 <emilianoar23@gmail.com>
2023-07-26 10:36:07 -0300

    Home responsive

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,37 +1,32 @@
-let eventCards = document.getElementById('eventCards')
-//console.log(eventCards)
-let fragment = document.createDocumentFragment()
+let eventCards = document.getElementById('eventCards');
+let fragment = document.createDocumentFragment();
 
 eventInfo.forEach((event) => {
-let dateEvent = new Date(event.date)
-let currentDate = new Date()
-let pastEvent = dateEvent < currentDate
-
-let card = `
-<div class="card m-4" style="width: 25rem; height: 40rem;">
-    <div class="card-image p-3">
-        <img src="${event.image}" class="card-img-top" alt="${event.name}">
-    </div>
-    <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        <p class="card-text">Category: ${event.category}</p>
-        <p class="card-text">Place: ${event.place}</p>
-        <p class="card-text">Capacity: ${event.capacity}</p>
-        <p class="card-text">Assistance: ${event.assistance || event.estimate}</p>
-        <div class="card-btn-section">
-            <p>Price ${event.price}</p>
-            <a href="./pages/details.html" class="btn btn-dark">More info</a>
+    let card = `
+        <div class="card m-3">
+            <div class="card-image p-3">
+                <img src="${event.image}" class="card-img-top" alt="${event.name}">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+                <p class="card-text">Category: ${event.category}</p>
+                <p class="card-text">Place: ${event.place}</p>
+                <p class="card-text">Capacity: ${event.capacity}</p>
+                <p class="card-text">Assistance: ${event.assistance || event.estimate}</p>
+                <div class="card-btn-section">
+                    <p>Price ${event.price}</p>
+                    <a href="./pages/details.html" class="btn btn-dark">More info</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-`;
+    `;
 
-let cardDiv = document.createElement('div')
-cardDiv.innerHTML = card
+    let cardDiv = document.createElement('div');
+    cardDiv.innerHTML = card;
 
-fragment.appendChild(cardDiv)
+    fragment.appendChild(cardDiv);
 });
 
-eventCards.innerHTML = ''
-eventCards.appendChild(fragment)
+eventCards.innerHTML = '';
+eventCards.appendChild(fragment);
